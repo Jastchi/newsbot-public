e53dc2bf9f65024d861cd1e4003102c99ce5e046
Jastchi <40759769+Jastchi@users.noreply.github.com>
2026-04-25 02:56:34 +0000

    Update: 2026-04-25

--- scripts/check-ignore-comments.sh
+++ scripts/check-ignore-comments.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 for file in "$@"; do
-  if grep -EH "(# type: ignore|# noqa|# ruff: ignore|# ignore)" "$file"; then
+  if grep -EH "(# type: ignore|# noqa|# ruff: ignore|# ruff: noqa|# ignore)" "$file"; then
     echo "Found type, linting, or general ignore comments in $file"
     echo "Please fix the underlying issue instead of suppressing warnings"
     exit 1
